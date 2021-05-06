c800f50b8f6a5f9854b63748fcc62b5114a9ab90
najla-coll <najla.yousfi@yahoo.com>
2021-05-06 15:29:07 +0100

    first

---
+++ myfirstapp/src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Form} from 'react-bootstrap';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Form>
+     <Form.Group controlId="exampleForm.ControlInput1">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="name@example.com" />
+      </Form.Group>
+     <Form.Group controlId="exampleForm.ControlSelect1">
+    <Form.Label>Example select</Form.Label>
+    <Form.Control as="select">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </Form.Control>
+  </Form.Group>
+  <Form.Group controlId="exampleForm.ControlSelect2">
+    <Form.Label>Example multiple select</Form.Label>
+    <Form.Control as="select" multiple>
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </Form.Control>
+        </Form.Group>
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Example textarea</Form.Label>
+         <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+        
+  <Form.Group controlId="formBasicRange">
+    <Form.Label>Range</Form.Label>
+    <Form.Control type="range" />
+  </Form.Group>
+  
+        </Form>
+      
+    </div>
+  );
+}
+
+export default App;
